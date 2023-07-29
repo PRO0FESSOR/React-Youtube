55f894075a610bd7188df2b0ac865d1b261c4357
Professor <siddharth96549@gmail.com>
2023-07-29 13:10:13 +0530

    state lifting

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import PlayButton from './components/PlayButton';
-import SampleVideo from './components/SampleVideo';
-import videosDb from './Data/data'
-import AddButton from './components/AddButton';
+import { useState } from "react";
+import "./App.css";
+import PlayButton from "./components/PlayButton";
+import SampleVideo from "./components/SampleVideo";
+import videosDb from "./Data/data";
+import AddButton from "./components/AddButton";
 
 function App() {
+  const [videos, setVideos] = useState(videosDb);
 
-  const[videos,setVideos] = useState(videosDb)
+  function addVideos(video) {
+    setVideos([...videos,{...video , id:videos.length+1}]);
+  }
 
   return (
     <>
-     <div className="App">
-      <AddButton></AddButton>
-      {videos.map((video) => (
-        <SampleVideo
-          key={video.id}
-          title={video.title}
-          views={video.views}
-          time={video.time}
-          channel={video.channel}
-          verified={video.verified}
-          id={video.id}
-        >
-          <PlayButton>
-            {video.title}
-          </PlayButton>
-        </SampleVideo>
-      ))}
-    </div>
+      <div className="App">
+        <AddButton addVideos={addVideos}></AddButton>
+        {videos.map((video) => (
+          <SampleVideo
+            key={video.id}
+            title={video.title}
+            views={video.views}
+            time={video.time}
+            channel={video.channel}
+            verified={video.verified}
+            id={video.id}
+          >
+            <PlayButton>{video.title}</PlayButton>
+          </SampleVideo>
+        ))}
+      </div>
     </>
   );
 }
--- youtube/src/components/AddButton.js
+++ youtube/src/components/AddButton.js
@@ -1,27 +1,51 @@
-import React from 'react'
-import './AddButton.css';
+import React, { useState } from "react";
+import "./AddButton.css";
 
-export default function AddButton() {
+const initialState = {
+  time: "1 month ago",
+  channel: "Coder Dost",
+  verified: true,
+  title:'',
+  views:'',
+}
+
+export default function AddButton({addVideos}) {
+  const [video, setVideo] = useState(initialState);
+
+  function handelSubmit(e)
+  {
+    e.preventDefault();
+    addVideos(video);
+    console.log(video); 
+    setVideo(initialState);
+  }
+
+  function handelChange(e) {
+    console.log(e.target.name, e.target.value);
+    setVideo({ ...video, [e.target.name]: e.target.value });
+  }
+
   return (
     <>
-    <form>
-        <input type="text" name="title" placeholder='Title'/>
-        <input type="text" name="views" placeholder='views' />
-        <button>Add Button</button>
-        {/* <button onClick={()=>{setVideos(
-          [...videos,
-            { id:videos.length+1,
-              title: 'Next JS tutorial',
-              views: '1M',
-              time: '1 month ago',
-              channel: 'Coder Dost',
-              verified: true            
-          }]
-        )}}>Add Videos</button> */}
-
-    </form>
+      <form>
+        <input
+          type="text"
+          name="title"
+          onChange={handelChange}
+          placeholder="Title"
+          value={video.title}
+        />
+        <input
+          type="text"
+          name="views"
+          onChange={handelChange}
+          placeholder="views"
+          value={video.views}
+        />
+        <button onClick={handelSubmit}>Add Button</button>
+      </form>
     </>
-  )
+  );
 }
 /* <div style={{color:'white'}} >
         
